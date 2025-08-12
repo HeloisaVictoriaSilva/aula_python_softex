b99bf56b2fb565f40738c12ca7acab2e3a7dbbf7
Heloisa Victoria <heloisavictor7@gmail.com>
2025-08-12 20:03:20 +0000

    if

---
+++ aula3/if.py
@@ -0,0 +1,16 @@
+# x = int(input("digite um numero: "))
+# y = int(input("digite outro numero: "))
+
+# if y != 0:
+#     print(x/y)
+# else:
+#     print("O valor informado para divisão não pode ser zero.")
+
+
+# if True:
+#     pass # pass é uma instrução que não faz nada, é um espaço reservado
+#     print("é verdade")
+# elif True:
+#     print("OK")
+# else:
+#     print("é falso")
