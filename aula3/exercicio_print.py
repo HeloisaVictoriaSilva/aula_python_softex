6b44689f221127e66b4db22ec852f431d681a346
Heloisa Victoria <heloisavictor7@gmail.com>
2025-08-12 19:25:06 +0000

    variaveis e exercicio

---
+++ aula3/exercicio_print.py
@@ -0,0 +1,64 @@
+#exercicio1
+ #print("ola mundo!")
+
+#exercicio2
+ #print("maria")
+ #print("Alan")
+ # print("sabrina")
+
+#exercicio3 e 4
+#nome = "joão"
+#idade = 20
+#curso = "Python basico"
+
+#print("nome:",  nome, "idade:", idade, "curso:", curso)
+
+# {} $
+
+#exercicio5 
+#numero = 4
+#desenho= "#"
+#print(numero * desenho)
+
+#numero = 1
+#desenho = "#"
+#print(numero * desenho, numero * desenho)
+
+#exercicio6
+#a = 10
+#b = 3
+#soma = a + b
+#subtracao = a - b
+#multiplicacao = a * b
+#divisao = a / b
+#divisao_inteira = a // b
+#modulo = a % b
+#exponencial = a ** b
+#print(f"soma: {soma}, subtracao: {subtracao}, mulpiplicacao: {multiplicacao}, divisao: {divisao}, divisao inteira: {divisao_inteira}, rsto da divisao: {modulo}, potenciacao: {exponencial}")
+
+# exercicio7
+# numero1 = int(input("digite o primeiro numero da operação:"))
+# numero2 = int(input("digite o segundo numero da operação:"))
+
+# soma = numero1 + numero2
+# subtracao = numero1 - numero2
+# multiplicacao = numero1 * numero2
+# divisao = numero1 / numero2
+
+# print(f"soma: {soma}") 
+# print(f"subtração: {subtracao}")
+# print(f"multiplicação: {multiplicacao}")
+# print(f"divisão: {divisao}")
+
+#exercicio9
+# numero1 =int(input("digite a largura:"))
+# numero2 = int(input("digite a altura:"))
+# area = numero1 * numero2
+# print(f"a area do retangulo é: {area}")
+
+#exercicio10
+# valor_da_compra = float(input("informe o valor da compra:"))
+# valor_pago = float(input("informe o valor pago:"))
+
+# resultado = valor_pago - valor_da_compra
+# print(f"o troco é: {resultado}")
---
+++ aula3/variavel.py
@@ -0,0 +1,16 @@
+#print(type("22")) #string
+#print(type(22.3)) #float
+#print(type(0)) #int
+#print(type(true)) #boolean
+#print(type(false)) #bolean
+#print(type(str(22))) #str para o dado se tornar string
+
+
+#input("informe o seu nome:") #input é uma função que recebe um valor ao dado do usuário
+
+#nome = int(input("informe o seu nome:")) #o int para o dado ser convertido para inteiro
+#print(type(int(nome)))# int para o dado ser convertido para inteiro
+
+#opereções // divisão sem resto, % modulo: sobra da divisão, ** exponencial
+#operadores logicos: == igual, != diferente, > maior, < menor, >= maior ou igual, <= menor ou igual 
+
