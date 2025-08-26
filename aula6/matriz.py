2b013cc2d3ac28e6783172d4e839ff248e03ec14
Heloisa Victoria <heloisavictor7@gmail.com>
2025-08-26 01:31:36 +0000

    matriz aula6

---
+++ aula6/matriz.py
@@ -0,0 +1,16 @@
+#matriz = é um objeto que contém uma lista de listas
+# matriz = [[1,2], 
+#           [3,4]]
+
+# criando matriz com list comprehesion
+# matriz1 = [[0 for i in range(3)] for x in range(3)]
+# print(matriz1)
+
+#criando matriz usando for
+# matriz2 = []
+# for i in range(3):
+#     sublista = []
+#     for x in range(3):
+#         sublista.append(x)
+# print(sublista[1])
+
