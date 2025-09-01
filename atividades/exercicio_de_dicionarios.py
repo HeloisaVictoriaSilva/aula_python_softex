8488fa913838230a9bcb6d3528bbf52285bbed37
Heloisa Victoria <heloisavictor7@gmail.com>
2025-09-01 03:24:52 +0000

    atividade dicionarios finalizada

--- atividades/exercicio_de_dicionarios.py
+++ atividades/exercicio_de_dicionarios.py
@@ -35,3 +35,72 @@ contato = {
 print(contato.get("telefone"))
 
 #6
+palavras = ["maçã", "banana", "maçã", "laranja", "banana", "maçã"]
+def contador(palavras):
+    frequencia = {}
+    for palavra in palavras:
+        if palavra in frequencia:
+            frequencia[palavra] += 1
+        else:
+            frequencia[palavra] = 1   
+    return frequencia                 
+
+
+print(contador(palavras))
+
+#7
+d = {
+    "a": 1,
+    "b": 2,
+    "c": 3
+}
+inverter = {}
+for chave, valor in d.items():
+    inverter[valor] = chave
+print(inverter)
+
+#8
+notas_alunos = {
+    "Ana": [7, 8, 9],
+    "Carlos": [6, 5, 7],
+    "Beatriz": [10, 9, 8]
+}
+for nome, notas in notas_alunos.items():
+    soma = 0
+    contador = 0
+    for nota in notas:
+        soma += nota
+        contador += 1
+    media = soma / contador
+    print(f"{nome} tem média {media}")
+
+#9
+def mesclar_dicts(d1, d2):
+    novo = d1.copy()  
+    novo.update(d2)  
+    return novo
+
+d1 = {
+      "a": 1, 
+      "b": 2, 
+      "c": 3
+}
+d2 = {
+      "b": 5, 
+      "d": 6
+}
+
+resultado = mesclar_dicts(d1, d2)
+print(resultado)
+
+#10
+pontuacoes = {
+              "João": 50, 
+              "Maria": 80, 
+              "Pedro": 70
+    }
+
+ordenado = sorted(pontuacoes.items(), key=lambda item: item[1], reverse=True)
+
+for nome, pontos in ordenado:
+    print(f"{nome}: {pontos}")
