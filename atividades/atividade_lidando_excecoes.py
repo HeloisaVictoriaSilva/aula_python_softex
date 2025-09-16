a865b1cfd239a3f2174567c199c9df39c87f205e
Heloisa Victoria <heloisavictor7@gmail.com>
2025-09-16 19:53:40 +0000

    atividade exceções ajustada

--- atividades/atividade_lidando_excecoes.py
+++ atividades/atividade_lidando_excecoes.py
@@ -22,13 +22,61 @@
 #     print(f"O resultado da multiplicação é: {multiplicação}")
 
 #3
+# try:
+#     numero_inteiro= int(input("Digite um número inteiro: "))
+# except (ValueError, TypeError):
+#     print("Os dados digitados não são um número inteiro")
+# except KeyboardInterrupt:
+#     print(" O usuário preferiu não digitar os dados solicitados")
+# except Exception as erro:
+#     print(f"Um erro foi encontrado {erro.__cause__}")
+# else:
+#     print("O dado digitado foi um número inteiro e a conversão ocorreu corretamente")
+
+#4
+# try:
+#     open("dados.txt", "r")
+# except FileNotFoundError:
+#     print("arquivo não existe")
+# finally:
+#     print("Encerrando programa")
+
+#5
+# def dividir(a,b):
+#     if b==0:
+#         raise ZeroDivisionError("O denominador não pode ser zero")
+#     elif b!=0:
+#         divisão= a/b
+#         print(divisão)
+#     #else:
+#        # print(divisão)
+    
+# dividir(int(input("Digite um número para o numerador: ")), int(input("Digite um número para o denominador: ")))
+
+#6
+# class IdadeInvalidaError(Exception):...
+
+# try:
+#     idade = int(input("Informe a idade: "))
+#     if idade <= 0:
+#         raise IdadeInvalidaError("idade não pode ser negativa ou zero")
+# except IdadeInvalidaError as erro:
+#     print(type(erro).__name__,":", erro)
+
+
+# def cadastrar_idade(idade):
+#     if idade <= 0:
+#       raise IdadeInvalidaError("A idade é negativa ou zero, digite uma idade valida")
+
+#7
 try:
-    numero_inteiro= int(input("Digite um número inteiro: "))
-except (ValueError, TypeError):
-    print("Os dados digitados não são um número inteiro")
-except KeyboardInterrupt:
-    print(" O usuário preferiu não digitar os dados solicitados")
-except Exception as erro:
-    print(f"Um erro foi encontrado {erro.__cause__}")
-else:
-    print("O dado digitado foi um número inteiro e a conversão ocorreu corretamente")
+    numerador= int(input("Digite um numerador: "))
+    denominador= int(input("Digite o denominador: "))
+    dividir= numerador/denominador
+    print(f"O resultado da divisão: {dividir}")
+except ValueError :
+    print("número invalido, digite um número valido")
+except ZeroDivisionError:
+    print("denominador não pode ser zero, digite outro número")
+
+#8
