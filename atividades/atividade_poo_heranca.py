e04c54b6128b678fa843fd2d37990021110dfb4c
Heloisa Victoria Silva <heloisavictor7@gmail.com>
2025-09-25 14:07:16 -0300

    atividade poo herança finalizada

--- atividades/atividade_poo_heranca.py
+++ atividades/atividade_poo_heranca.py
@@ -1,39 +1,40 @@
-#1
-# class Usuario:
-#     def __init__(self,nome, email):
-#         self.nome = nome
-#         self.email = email
-#     def exibir_informacoes(self):
-#         return f"Nome do úsuario: {self.nome}, email: {self.email}"
-#     def saudacao(self):
-#         print(f"Olá usuario {self.nome}")
-# class Cliente(Usuario):
-#     def __init__(self, nome, email, saldo):
-#         super().__init__(nome, email)
-#         self.saldo = saldo
-#     def exibir_informacoes(self):
-#         return super().exibir_informacoes()
-#     def saudacao(self):
-#         return f"Olá Cliente {self.nome}"
-#     # def __str__(self):
-#     #     return f"Nome do úsuario: {self.nome}, email: {self.email}"
+#1, 2, 3, 4
+class Usuario:
+    def __init__(self,nome, email):
+        self.nome = nome
+        self.email = email
+    def exibir_informacoes(self):
+        return f"Nome do úsuario: {self.nome}, email: {self.email}"
+    def saudacao(self):
+        print(f"Olá usuario {self.nome}")
+class Cliente(Usuario):
+    def __init__(self, nome, email, saldo):
+        super().__init__(nome, email)
+        self.saldo = saldo
+    def exibir_informacoes(self):
+        return super().exibir_informacoes()
+    def saudacao(self):
+        return f"Olá Cliente {self.nome}"
+    # def __str__(self):
+    #     return f"Nome do úsuario: {self.nome}, email: {self.email}"
 
-# class Funcionario(Usuario):
-#     def __init__(self, nome, email):
-#         super().__init__(nome, email)
+#5, 6, 7
+class Funcionario(Usuario):
+    def __init__(self, nome, email):
+        super().__init__(nome, email)
 
-# class Gerente(Funcionario):
-#     def __init__(self, nome, email):
-#         super().__init__(nome, email) 
+class Gerente(Funcionario):
+    def __init__(self, nome, email):
+        super().__init__(nome, email) 
 
-# cliente1 = Cliente("Heloisa", "heloisa@gmail.com", 654)
-# usuario1 = Usuario("Ana", "ana@gmail.com")
-# gerente1 = Gerente("Carolina", "carolina@gmail.com")
-# print(usuario1.exibir_informacoes())
-# print(cliente1.exibir_informacoes())
-# print(cliente1.saudacao())
-# print(usuario1.saudacao())
-# print(gerente1.exibir_informacoes())
+cliente1 = Cliente("Heloisa", "heloisa@gmail.com", 654)
+usuario1 = Usuario("Ana", "ana@gmail.com")
+gerente1 = Gerente("Carolina", "carolina@gmail.com")
+print(usuario1.exibir_informacoes())
+print(cliente1.exibir_informacoes())
+print(cliente1.saudacao())
+print(usuario1.saudacao())
+print(gerente1.exibir_informacoes())
 
 #6
 class  Autenticacao:
