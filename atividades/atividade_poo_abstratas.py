c97b461a85527f8d7574ea0170f6c563567ca3e0
Heloisa Victoria <heloisavictor7@gmail.com>
2025-10-14 21:54:35 +0000

    atividade poo classe abstrata

--- atividades/atividade_poo_abstratas.py
+++ atividades/atividade_poo_abstratas.py
@@ -20,14 +20,14 @@ from abc import ABC, abstractmethod
 # print(mel.falar())
 
 #2
-class Animal (ABC):
-     @abstractmethod
-     def falar():
-         print("AU AU")
+# class Animal (ABC):
+#      @abstractmethod
+#      def falar():
+#          print("AU AU")
 
-cachorro= Animal()
-print(cachorro.falar)
-#A classe abstrata não pode ser implementada, precisa de uma outra classe concreta para isso
+# cachorro= Animal()
+# print(cachorro.falar)
+# #A classe abstrata não pode ser implementada, precisa de uma outra classe concreta para isso
 
 #3
 class FormaGeometrica(ABC):
@@ -41,5 +41,52 @@ class FormaGeometrica(ABC):
 class Retangulo(FormaGeometrica):
      def area(self, x, y):
           area_retanulo= x * y
-          return super().area()
+          return area_retanulo
+     def perimetro(self,x,y):
+          perimetro_retangulo= 2 * (x + y)
+          return perimetro_retangulo
      
+retangulo= Retangulo()
+print(retangulo.area(5,3))
+
+print(retangulo.perimetro(5,3))
+
+#4
+class Trasnporte(ABC):
+     @abstractmethod
+     def mover():
+          pass
+     @abstractmethod
+     def parar():
+          pass
+
+class Carro(Trasnporte):
+     def mover(self):
+          return "Correndo"
+     
+kiwid= Carro()
+print(kiwid.mover())
+
+# A classe carro herda uma classe e metodos abstrados então 
+# como não foi implementadon todos od métodod abstrados, 
+# enquanto todos os métodos abstratos não forem instanciados 
+# a classe não pode ser implementada
+
+#codigo corrigida:
+class Trasnporte(ABC):
+     @abstractmethod
+     def mover():
+          pass
+     @abstractmethod
+     def parar():
+          pass
+
+class Carro(Trasnporte):
+     def mover(self):
+          return "Correndo"
+     def parar(self):
+          return "Parado"
+     
+kiwid= Carro()
+print(kiwid.mover())
+print(kiwid.parar())
