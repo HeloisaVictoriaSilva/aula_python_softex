b9abf781d05f4c290875861932392d03bd4937cc
Heloisa Silva <heloisavictor7@gmail.com>
2025-10-28 16:39:50 -0300

    atividade sql1 iniciada

--- bancodados/atividade_sql1.sql
+++ bancodados/atividade_sql1.sql
@@ -1,2 +1,24 @@
+.tables
+.schema Aluno
 ---1
-SELECT
+SELECT * FROM Aluno;
+---2
+SELECT 
+    nome,
+    nota1
+FROM Aluno;
+
+---3
+SELECT nome, nota1
+FROM Aluno
+WHERE nota1 >= 8;
+
+---4
+SELECT nome, data_nascimento
+FROM Aluno
+WHERE data_nascimento >= 2000;
+
+---5
+SELECT nome, mensalidade
+FROM Aluno
+WHERE mensalidade > 600;
