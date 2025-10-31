ad48b790a7c7bbf4975a4aa1905fd5f63934b574
Heloisa Victoria <heloisavictor7@gmail.com>
2025-10-30 21:47:47 -0300

    atividade sql1 finalizada

--- bancodados/atividade_sql1.sql
+++ bancodados/atividade_sql1.sql
@@ -1,5 +1,8 @@
 .tables
 .schema Aluno
+.schema Turma
+.schema Curso
+
 ---1
 SELECT * FROM Aluno;
 ---2
@@ -20,5 +23,34 @@ WHERE data_nascimento >= 2000;
 
 ---5
 SELECT nome, mensalidade
-FROM Aluno
+FROM Curso
 WHERE mensalidade > 600;
+
+---6
+SELECT nome, ano
+FROM Turma
+ORDER BY ano ASC;
+
+---7
+SELECT ano, 
+COUNT() AS quantidade_turmas
+FROM Turma
+GROUP BY ano;
+
+---8
+SELECT id_turma,
+SUM(nota1) / COUNT(nota1) AS media_nota1
+FROM Aluno
+GROUP BY id_turma;
+
+---9
+SELECT ano,
+COUNT() AS quantidade_turmas
+FROM Turma
+GROUP BY ano
+HAVING quantidade_turmas > 2;
+
+---10
+SELECT nome, mensalidade
+FROM Curso
+ORDER BY mensalidade DESC;
