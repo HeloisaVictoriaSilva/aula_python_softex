bbb8e864fa0d09d93e7c0252ff776290717cf25d
Heloisa Victoria <heloisavictor7@gmail.com>
2025-11-08 16:59:05 -0300

    atividade sqlite finalizada

--- bancodados/atividade_sqlite_py.py
+++ bancodados/atividade_sqlite_py.py
@@ -6,7 +6,7 @@ db_connection= sqlite3.connect('bancodados/escola_v2.db')
 
 cursor= db_connection.cursor()
 
-#2
+# #2
 cursor.execute("""
 SELECT * 
 FROM Aluno
@@ -16,5 +16,34 @@ query_result = cursor.fetchall()
 print(query_result)
 
 #3
+cursor.execute("""
+SELECT nota1, nota2,
+(nota1+nota2)/2 AS media
+FROM Aluno   
+ORDER BY media DESC
+LIMIT 10            
+               """)
+
+query_result = cursor.fetchall()
+print(query_result)
+
+#4
+cursor.execute("""
+SELECT *
+FROM Aluno
+LEFT JOIN Turma ON Aluno.id = Turma.id                       
+               """)
+query_result = cursor.fetchall()
+print(query_result)
+
+#5
+cursor.execute("""
+SELECT *
+FROM Aluno
+LEFT JOIN Turma ON Aluno.id = Turma.id
+WHERE Turma.id = 2                       
+               """)
+query_result = cursor.fetchall()
+print(query_result)
 
 db_connection.close()
